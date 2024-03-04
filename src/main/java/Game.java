1ea6c063319374686b8582c00f39fe6b5e4a766f
khalifa <oriyomibamikole@gmail.com>
2024-03-04 10:05:04 +0000

    added user input collection functionality

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,5 +2,14 @@ import java.util.Scanner;
 
 public class Game {
     private Scanner userInput;
-    public Game() {}
+    public Game() {
+        userInput = new Scanner(System.in);
+    }
+    public void start() {
+        int num1, num2;
+        System.out.println("Enter coordinates");
+        num1 = userInput.nextInt();
+        num2 = userInput.nextInt();
+        System.out.println(num1 + " " + num2);
+    }
 }
