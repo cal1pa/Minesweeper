5a240c0c70ad17e150d4bdee0e85f1ff1c930c3a
khalifa <oriyomibamikole@gmail.com>
2024-03-04 10:21:42 +0000

    added position class for handling user input

---
+++ src/main/java/Position.java
@@ -0,0 +1,21 @@
+public class Position {
+    public int row;
+    public int col;
+
+    // Constructor
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    /*
+ Getters
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+*/
+}
